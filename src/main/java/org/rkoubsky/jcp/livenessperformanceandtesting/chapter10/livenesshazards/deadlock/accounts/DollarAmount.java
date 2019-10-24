f5e0903cd778fdc591b091fe338825623430a117
Radek Koubsky <radekkoubsky@gmail.com>
2019-10-24 16:42:58 +0200

    Chapter10 - liveness hazards, deadlocks, open calls.

---
+++ src/main/java/org/rkoubsky/jcp/livenessperformanceandtesting/chapter10/livenesshazards/deadlock/accounts/DollarAmount.java
@@ -0,0 +1,21 @@
+package org.rkoubsky.jcp.livenessperformanceandtesting.chapter10.livenesshazards.deadlock.accounts;
+
+public class DollarAmount implements Comparable<DollarAmount> {
+    // Needs implementation
+
+    public DollarAmount(final int amount) {
+    }
+
+    public DollarAmount add(final DollarAmount d) {
+        return null;
+    }
+
+    public DollarAmount subtract(final DollarAmount d) {
+        return null;
+    }
+
+    @Override
+    public int compareTo(final DollarAmount dollarAmount) {
+        return 0;
+    }
+}
---
+++ src/main/java/org/rkoubsky/jcp/livenessperformanceandtesting/chapter10/livenesshazards/deadlock/accounts/InsufficientFundsException.java
@@ -0,0 +1,4 @@
+package org.rkoubsky.jcp.livenessperformanceandtesting.chapter10.livenesshazards.deadlock.accounts;
+
+public class InsufficientFundsException extends Exception {
+}
