af64ebe63665192f356268bc73a7bee7926a0286
Radek Koubsky <radekkoubsky@gmail.com>
2019-11-04 11:04:28 +0100

    Chapter13 - explicit locks, timed, polled and interruptible lock acquisition, read-write locks.

---
+++ src/main/java/org/rkoubsky/jcp/part4/advancedtopics/chapter13/explicitlocks/reentrantlock/lockacquisition/polled/Account.java
@@ -0,0 +1,17 @@
+package org.rkoubsky.jcp.part4.advancedtopics.chapter13.explicitlocks.reentrantlock.lockacquisition.polled;
+
+import java.util.concurrent.locks.Lock;
+
+public class Account {
+    public Lock lock;
+
+    void debit(final DollarAmount d) {
+    }
+
+    void credit(final DollarAmount d) {
+    }
+
+    DollarAmount getBalance() {
+        return null;
+    }
+}
---
+++ src/main/java/org/rkoubsky/jcp/part4/advancedtopics/chapter13/explicitlocks/reentrantlock/lockacquisition/polled/InsufficientFundsException.java
@@ -0,0 +1,4 @@
+package org.rkoubsky.jcp.part4.advancedtopics.chapter13.explicitlocks.reentrantlock.lockacquisition.polled;
+
+public class InsufficientFundsException extends Exception {
+}
